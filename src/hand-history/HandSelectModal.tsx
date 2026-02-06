054804d5c9ac3db5f55e01b9d627b48cee6c7562
松原啓斗 <matsubara@crebo.co.jp>
2026-02-06 18:57:11 +0900

    HandSelectModal内にカードcardList定義

--- src/hand-history/HandSelectModal.tsx
+++ src/hand-history/HandSelectModal.tsx
@@ -3,11 +3,30 @@ type Props = {
 };
 
 export const HandSelectModal = ({ onClose }: Props) => {
+  const suit = ["s", "h", "d", "c"];
+  const rank = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "T",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+  ];
+  const cardList = rank.map((r) => suit.map((s) => `${r}${s}`)).flat();
+  console.log(cardList);
+
   return (
     <div className="fixed inset-0 z-50">
       <div className="absolute inset-0 bg-black/40" onClick={() => onClose()} />
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-xl p-6 shadow-lg">
-        <h2 className="font-bold mb-4">Hand モーダル</h2>
+        <h2 className="font-bold mb-4">HandSelect</h2>
         <>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</>
         <button
           type="button"
