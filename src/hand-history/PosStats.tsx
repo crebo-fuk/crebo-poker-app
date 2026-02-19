e08c18497324431338840adc32e9c23155f9a3aa
松原啓斗 <matsubara@crebo.co.jp>
2026-02-19 15:17:13 +0900

    ポジション別収支用にTableHead追加。6/9Max表示ボタンUI変更

--- src/hand-history/PosStats.tsx
+++ src/hand-history/PosStats.tsx
@@ -33,20 +33,44 @@ export const PosStats = ({ hands }: Props) => {
       <h2 className="text-lg font-bold text-center mt-2 mb-3">
         ポジション別収支
       </h2>
-      {selectedPosMax === null && (
-        <div className="flex gap-3 items-center justify-center">
-          <button
-            className="border px-2 py-1"
-            onClick={() => setSelectedPosMax("6")}
-          >
-            6Max
-          </button>
-          <button
-            className="border px-2 py-1"
-            onClick={() => setSelectedPosMax("9")}
-          >
-            9Max
-          </button>
+      <div className="flex gap-3 items-center justify-center">
+        <button
+          className={`border px-2 py-1 ${selectedPosMax === "6" && "bg-gray-300"}`}
+          onClick={() => setSelectedPosMax("6")}
+        >
+          6Max
+        </button>
+        <button
+          className={`border px-2 py-1 ${selectedPosMax === "9" && "bg-gray-300"}`}
+          onClick={() => setSelectedPosMax("9")}
+        >
+          9Max
+        </button>
+      </div>
+      {selectedPosMax === "6" && (
+        <div>
+          <Table>
+            <TableHead>
+              <TableCell>Position</TableCell>
+              <TableCell>Hands</TableCell>
+              <TableCell>Total BB</TableCell>
+              <TableCell>Avg BB</TableCell>
+              <TableCell>BB/100</TableCell>
+            </TableHead>
+          </Table>
+        </div>
+      )}
+      {selectedPosMax === "9" && (
+        <div>
+          <Table>
+            <TableHead>
+              <TableCell>Position</TableCell>
+              <TableCell>Hands</TableCell>
+              <TableCell>Total BB</TableCell>
+              <TableCell>Avg BB</TableCell>
+              <TableCell>BB/100</TableCell>
+            </TableHead>
+          </Table>
         </div>
       )}
     </div>
