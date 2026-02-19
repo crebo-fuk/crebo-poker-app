fef8e473a4999e4c31bd115cde7a87260c002e31
松原啓斗 <matsubara@crebo.co.jp>
2026-02-19 12:40:34 +0900

    ポジション別収支で6Maxと9Max用のボタン追加（state管理）

--- src/hand-history/PosStats.tsx
+++ src/hand-history/PosStats.tsx
@@ -1,15 +1,45 @@
 import type { HandItem } from "../types/type";
+import { useState } from "react";
 
 type Props = {
   hands: HandItem[];
 };
 
 export const PosStats = ({ hands }: Props) => {
+  const [selectedPosMax, setSelectedPosMax] = useState<"6" | "9" | null>(null);
+  const positions9Max: string[] = [
+    "UTG",
+    "UTG+1",
+    "MP",
+    "MP+1",
+    "HJ",
+    "CO",
+    "BTN",
+    "SB",
+    "BB",
+  ];
+  const positions6Max: string[] = ["UTG", "MP", "CO", "BTN", "SB", "BB"];
   return (
     <div>
       <h2 className="text-lg font-bold text-center mt-2 mb-3">
         ポジション別収支
       </h2>
+      {selectedPosMax === null && (
+        <div className="flex gap-3 items-center justify-center">
+          <button
+            className="border px-2 py-1"
+            onClick={() => setSelectedPosMax("9")}
+          >
+            9Max
+          </button>
+          <button
+            className="border px-2 py-1"
+            onClick={() => setSelectedPosMax("6")}
+          >
+            6Max
+          </button>
+        </div>
+      )}
     </div>
   );
 };
